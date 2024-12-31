add2c654f95561d2d5b3253212053a513cabe08e
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2024-12-30 16:19:21 -0800

    File name changed for hosting

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles/GlobalStyles.css'
+import './styles/GlobalStyles'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
